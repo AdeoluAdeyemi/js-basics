4304b335f7be77419d201e00dca4d6951d00dda7
Adeolu Adeyemi <adeolu.ooa@gmail.com>
2022-02-05 14:15:45 +0100

    Javascript introduction: primitive and reference types

---
+++ index.js
@@ -0,0 +1,44 @@
+//This is my first Javascript Code!
+console.log('Hello World');
+
+//Primitive Data Types
+let name = 'Adeolu';
+let age = 29;
+let isApproved = true;
+let firstName = undefined;
+let selectedBrand = null;
+
+
+// Reference Data Types
+
+
+//Object
+let person = {
+    name: 'Adeolu',
+    age: 29
+};
+
+console.log(person.name);
+
+console.log(person['age']);
+
+
+//Array
+let selectedColor = ['red','green','blue',1];
+console.log(selectedColor[0]);
+console.log(selectedColor[1]);
+console.log(selectedColor.length);
+
+
+//Function
+//Performing a task;
+function greet(name,lastName){
+    console.log('Hello '+ name+ ' of ' + lastName);
+}
+
+//Calculates a value;
+function square(number){
+    return number*number;
+}
+
+console.log
